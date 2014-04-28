27f8c6da774ea2973c0a9fc98d0ad2887788fa6d
markent94 <mrb600@hawaii.edu>
2014-04-27 15:56:38 -1000

    Update test.java

--- test.java
+++ test.java
@@ -1,28 +1,50 @@
-public class test{
-   public static void main(String[] args) {
-      System.out.println("test. TEST!");
-      
-package ics321proj;
 
-public class Items {
+public class Item {
 	
-	private String name;
-	private int attack;
-	private int health;
-	private int defense;
-	private int block;
-	private int crit;
+	private int userID;
+	private int itemID;
+	private String location;
 	
-	public Items() {
-		name = null;
-		attack = 0;
-		health = 0;
-		defense = 0;
-		block = 0;
-		crit = 0;	
+	public Item(int userID, String location) {
+		this.userID = userID;
+		this.location = location;
+		this.itemID = 0;
+	}
+	
+	public int getItemID() {
+		return this.itemID;
+	}
+	
+	public void createItem(int iQuality, int iType) {
+		//get all items with given item quality and type
+		//for sql SELECT * FROM iPREFIXES WHERE itemQuality = iQuality AND prefixType = iType);
+		String pName; //from ^
+		String pATK; // ^
+		String pdef; // ^
+		String pcrit; // ^
+		String pblock; // ^
+		//SELECT * FROM iSUFFIXES WHERE itemQuality = iQuality AND suffixType = iType);
+		String sName; //from ^
+		String sATK; // ^
+		String sdef; // ^
+		String scrit; // ^
+		String sblock; // ^
+		//SELECT * FROM iNAMES WHERE itemQuality = iQuality AND nameType = iType);
+		String iName; //from ^
+		String iATK; // ^
+		String idef; // ^
+		String icrit; // ^
+		String iblock; //^
+		//All these are nested under 
+		//SELECT column FROM table
+		//ORDER BY RAND()
+		//LIMIT 1
+		//This command gets a randomized item with the matching itemquality and itemtype
+		String tName = pName + iName +sName;
+		String tATK = pATK + iATK + sATK;
+		String tdef = pdef + idef + sdef;
+		String tcrit = pdef + idef + sdef;
+		String tblock = pblock + iblock + sblock;
+		//INSERT into items table, userID = userID, itemID = ??, itemName = tname...., location = ??
 	}
 }
-
-   }
-}
-      
